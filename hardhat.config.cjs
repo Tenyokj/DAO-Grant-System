ca1b0e3e456846c6e2381dc4ff1eaf83eca65ec3
Tenyokj <dv842449@gmail.com>
2025-10-10 11:45:13 +0300

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -4,9 +4,9 @@ require("dotenv").config();
 
 module.exports = {
   solidity: {
-    version: "0.8.25", // Ваш компилятор Solidity
+    version: "0.8.25",
     settings: {
-      evmVersion: "cancun", // Указываем Cancun для EVM
+      evmVersion: "cancun",
       optimizer: {
         enabled: true,
         runs: 1000,
@@ -16,7 +16,7 @@ module.exports = {
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
-      chainId: 31337,//1337
+      chainId: 31337,
     },
   },
 };
